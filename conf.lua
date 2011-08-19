b08e8a3180ac808e2fbe1c8d7dada408b591bf5f
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2011-08-19 18:10:47 +0530

    First, default commit.

---
+++ conf.lua
@@ -0,0 +1,15 @@
+function love.conf(t)
+    t.title = "LD48 #21 Entry by Scio"
+    t.screen.vsync = true
+    t.screen.fsaa = 2
+    t.modules.joystick = false
+    --t.modules.audio = true
+    --t.modules.keyboard = true
+    --t.modules.event = true
+    --t.modules.image = true
+    --t.modules.graphics = true
+    --t.modules.timer = true
+    --t.modules.mouse = true
+    --t.modules.sound = true
+    --t.modules.physics = true
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+function love.load()
+
+    --setup
+    width = 800
+    height = 450
+    fullscreen = false
+    love.graphics.setMode(width, height, fullscreen)
+    PI = 3.14139
+
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
